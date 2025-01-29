6692b5524a264fa87a1bbbe1b4ba2e8941f663c3
kaustubh <kaustubhkumbhare02@gmail.com>
2025-01-29 19:53:44 +0000

    actix

---
+++ actix-rhai/src/main.rs
@@ -0,0 +1,23 @@
+use std::path::{self, Path};
+
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use rhai::Engine;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(multiply).service(add))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+#[get("/add/{num1}/{num2}")]
+async fn add(path: Path<(i64, i64)>) -> impl Responder {
+    let (num1, num2) = path.into_inner();
+    HttpResponse::Ok().body(num1 + num2)
+}
+
+#[get("/multiply/{num1}/{num2}")]
+async fn multiply() -> impl Responder {
+    HttpResponse::Ok().body(num1 * num2)
+}
