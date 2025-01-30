db13e230c0bc3a00319616282bf5239622193d77
kaustubh <kaustubhkumbhare02@gmail.com>
2025-01-30 04:38:26 +0000

    actix and rhai

--- actix-rhai/src/main.rs
+++ actix-rhai/src/main.rs
@@ -12,12 +12,27 @@ async fn main() -> std::io::Result<()> {
 }
 
 #[get("/add/{num1}/{num2}")]
-async fn add(path: Path<(i64, i64)>) -> impl Responder {
+async fn add(path: web::Path<(i64, i64)>) -> impl Responder {
     let (num1, num2) = path.into_inner();
-    HttpResponse::Ok().body(num1 + num2)
+    println!("num1: {}, num2: {}", num1, num2);
+
+    let mut engine = Engine::new();
+    engine.register_fn("num1", move || num1);
+    engine.register_fn("num2", move || num2);
+
+    let result = engine.eval_file::<i64>("src/add.rhai".into()).unwrap();
+
+    format!("{}", result)
 }
 
 #[get("/multiply/{num1}/{num2}")]
-async fn multiply() -> impl Responder {
-    HttpResponse::Ok().body(num1 * num2)
+async fn multiply(path: web::Path<(i64, i64)>) -> impl Responder {
+    let (num1, num2) = path.into_inner();
+    let mut engine = Engine::new();
+    engine.register_fn("num1", move || num1);
+    engine.register_fn("num2", move || num2);
+
+    let result = engine.eval_file::<i64>("src/multiply.rhai".into()).unwrap();
+
+    format!("{}", result)
 }
